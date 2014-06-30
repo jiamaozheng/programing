78b05d2df191dedf14d3802400427b986706309e
guofenzhao <guofenzhao@Macintosh-5.local>
2014-06-29 19:59:37 -0500

    java programing

---
+++ EricZheng.java
@@ -0,0 +1,7 @@
+public class EricZheng {
+    public static void main(String[] args)
+
+    {
+	System.out.println("This is Eric Zheng");
+    }
+}
---
+++ Jiamao.java
@@ -0,0 +1,8 @@
+public class Jiamao {
+
+    public static void main(String[] args)
+    {
+	System.out.println("This is Jiamao Zheng");
+}
+
+}
