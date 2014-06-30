95abccf613aa1acbbd685c0b8457a385aaee1a1f
Jiamao Zheng <jiamaoz@yahoo.com>
2014-06-29 20:40:48 -0500

    Emily is on branch

--- EmilyZheng.java
+++ EmilyZheng.java
@@ -1,6 +1,6 @@
 public class EmilyZheng {
     public static void main(String[] args)
     {
-	System.out.println("This is Emily Zheng");
+	System.out.println("This is Emily Zheng, and she has two brothers");
     }
 }
